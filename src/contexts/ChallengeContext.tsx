348764ddec1ff956d28ec2755f700b16a8f9d5d5
Valter Silva <valtersilvajr@live.com>
2021-04-03 16:51:15 -0300

    [NEW]: cookies configuration and modal creation

--- src/contexts/ChallengeContext.tsx
+++ src/contexts/ChallengeContext.tsx
@@ -1,5 +1,7 @@
 import { createContext, useState, ReactNode, useEffect } from 'react';
+import Cookies from 'js-cookie';
 import challenges from '../../challenges.json';
+import { LevelUpModal } from '../components/LevelUpModal';
 
 interface IChallenge {
     type: 'body' | 'eye',
@@ -17,20 +19,25 @@ interface IChallengesContextData {
     completeChallenge(): void;
     levelUp(): void;
     startNewChallenge(): void;
+    closeLevelUpModal(): void;
 }
 
 interface IChallegeProviderProps {
     children: ReactNode;
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
 }
 
 export const ChallengesContext = createContext({} as IChallengesContextData);
 
-export function ChallengesProvider({ children }: IChallegeProviderProps) {
-    const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(0);
-    const [challengesCompleted, setChallengesCompleted] = useState(0);
+export function ChallengesProvider({ children, ...rest }: IChallegeProviderProps) {
+    const [level, setLevel] = useState(rest.level ?? 1);
+    const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
+    const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
 
     const [activeChallenge, setActiveChallenge] = useState(null);
+    const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false);
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2 );
 
@@ -38,8 +45,19 @@ export function ChallengesProvider({ children }: IChallegeProviderProps) {
         Notification.requestPermission();
     }, []);
 
+    useEffect(() => {
+        Cookies.set('level', String(level));
+        Cookies.set('currentExperience', String(currentExperience));
+        Cookies.set('challengesCompleted', String(challengesCompleted));
+    },[level, currentExperience, challengesCompleted])
+
     function levelUp() {
         setLevel(level + 1);
+        setIsLevelUpModalOpen(true);
+    }
+
+    function closeLevelUpModal(){
+        setIsLevelUpModalOpen(false);
     }
 
     function startNewChallenge(){
@@ -91,9 +109,11 @@ export function ChallengesProvider({ children }: IChallegeProviderProps) {
                 startNewChallenge,
                 completeChallenge,
                 resetChallenge,
+                closeLevelUpModal,
             }}
         >
             {children}
+            { isLevelUpModalOpen && <LevelUpModal />}
         </ChallengesContext.Provider>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,61 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import React from 'react';
 import ChallengeBox from '../components/ChallengeBox';
 import CompletedChallenges from "../components/CompletedChallenges";
 import Container from "../components/Container";
 import Countdown from "../components/Countdown";
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from "../components/Profile";
+import { ChallengesProvider } from '../contexts/ChallengeContext';
 import { CountdownProvider } from '../contexts/CountdownContext';
 import { Section } from "./Home/styles";
 
-export default function Home() {
-  return (
-    <Container>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
 
-      <ExperienceBar />
-      
-      <CountdownProvider>
-        <Section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </Section>
-      </CountdownProvider>
-    </Container>
+export default function Home(props: HomeProps) {  
+  return (
+    <ChallengesProvider 
+      level={props.level} 
+      currentExperience={props.currentExperience} 
+      challengesCompleted={props.challengesCompleted}
+    >
+      <Container>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
+
+        <ExperienceBar />
+        
+        <CountdownProvider>
+          <Section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </Section>
+        </CountdownProvider>
+      </Container>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+    
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    }
+  }
 }
